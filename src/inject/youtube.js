219e200e6aab0f89c38c8f92958514e2ee6cea14
James Zhu <james.zhu.engineer@gmail.com>
2017-02-18 15:19:55 -0800

    Implement youtube pausing

--- src/inject/youtube.js
+++ src/inject/youtube.js
@@ -12,58 +12,78 @@
  * everytime a webpage loads over HTTP or HTTPS.
  */
 
-chrome.extension.sendMessage({}, function(response) {
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+function pauseYoutube() {
+	document.getElementsByClassName("ytp-play-button")[0].click();
+}
+
+chrome.extension.sendMessage({}, async function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 		if (document.readyState === "complete") {
 			// This part of the script triggers when page is done loading
 			clearInterval(readyStateCheckInterval);
-			chrome.storage.local.get('settings', function(response) {
-				var replacementRules = null;
-				if (typeof chrome.runtime.lastError === 'undefined') {
-					var settings = response.settings;
-					lines = settings.split("\n").filter(function(line) {
-						return line.indexOf("->") != -1;
-					});
-					replacementRules = parseSettings(lines);
-				}
-				console.log(replacementRules);
-				walk(document.body, replacementRules);
-				replaceAllImages(replacementRules)
-			});
+			sleep(5000).then(pauseYoutube);
 		}
 	}, 10);
 });
 
-// Update a text node's contents
-function handleText(textNode, settings)
-{
-	var text = textNode.nodeValue;
-	var modifiedText = modifyText(text, settings);
-	textNode.nodeValue = modifiedText;
+
+function createAudioMeter(audioContext,clipLevel,averaging,clipLag) {
+	var processor = audioContext.createScriptProcessor(512);
+	processor.onaudioprocess = volumeAudioProcess;
+	processor.clipping = false;
+	processor.lastClip = 0;
+	processor.volume = 0;
+	processor.clipLevel = clipLevel || 0.98;
+	processor.averaging = averaging || 0.95;
+	processor.clipLag = clipLag || 750;
+
+	// this will have no effect, since we don't copy the input to the output,
+	// but works around a current Chrome bug.
+	processor.connect(audioContext.destination);
+
+	processor.checkClipping =
+		function(){
+			if (!this.clipping)
+				return false;
+			if ((this.lastClip + this.clipLag) < window.performance.now())
+				this.clipping = false;
+			return this.clipping;
+		};
+
+	processor.shutdown =
+		function(){
+			this.disconnect();
+			this.onaudioprocess = null;
+		};
+
+	return processor;
 }
 
-// Full credit to: http://is.gd/mwZp7E
-function walk(node, settings)
-{
-	var child, next;
+function volumeAudioProcess( event ) {
+	var buf = event.inputBuffer.getChannelData(0);
+    var bufLength = buf.length;
+	var sum = 0;
+    var x;
 
-	// For more info, read http://www.w3schools.com/jsref/prop_node_nodetype.asp
-	switch (node.nodeType)
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while (child)
-			{
-				next = child.nextSibling;
-				walk(child, settings);
-				child = next;
-			}
-			break;
+	// Do a root-mean-square on the samples: sum up the squares...
+    for (var i=0; i<bufLength; i++) {
+    	x = buf[i];
+    	if (Math.abs(x)>=this.clipLevel) {
+    		this.clipping = true;
+    		this.lastClip = window.performance.now();
+    	}
+    	sum += x * x;
+    }
 
-		case 3: // Text node
-			handleText(node, settings);
-			break;
-	}
+    // ... then take the square root of the sum.
+    var rms =  Math.sqrt(sum / bufLength);
+
+    // Now smooth this out with the averaging factor applied
+    // to the previous sample - take the max here because we
+    // want "fast attack, slow release."
+    this.volume = Math.max(rms, this.volume*this.averaging);
 }
